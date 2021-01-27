2e826803cc066d4922417243654a8e6d00bdfa7c
Hugo <hugoattal@hotmail.fr>
2021-01-27 01:05:44 +0100

    Add nginx and https support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,10 @@
 import "module-alias/register";
 import { connectDatabase } from "@/database";
 import * as Fastify from "fastify";
-import FastifyStatic from "fastify-static";
 import cors from "cors";
 import Middie from "middie";
 
 import ProductHandler from "@/modules/product/handler";
-import * as path from "path";
 
 async function init() {
     const isDevelopment = (process.env.NODE_ENV === "development");
@@ -21,15 +19,7 @@ async function init() {
 
     await connectDatabase();
 
-    server.register(FastifyStatic, {
-        root: path.join(__dirname, "../../frontend/dist")
-    });
-
-    server.setNotFoundHandler((_req, res) => {
-        res.sendFile("index.html");
-    });
-
-    server.register(ProductHandler, { prefix: "/api/products" });
+    server.register(ProductHandler, { prefix: "/products" });
 
     return server;
 }
---
+++ system/readme.md
@@ -0,0 +1,58 @@
+# System
+
+## Nginx
+
+Installation
+```
+apt-get install nginx
+```
+
+Make symlinks
+```
+ln -s /home/orbital-market/system/nginx/orbital-market.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/orbital-market.conf /etc/nginx/sites-enabled/
+```
+
+Test configuration
+```
+nginx -t
+```
+
+Start service
+```
+service nginx start
+service nginx restart
+service nginx stop
+```
+
+## PM2
+
+Installation
+```
+npm install -g pm2
+```
+
+Running
+```
+pm2 start npm --name orbital-market-api -- start
+```
+
+Hot reload (zero downtime)
+```
+pm2 reload orbital-market-api
+```
+
+List and monitoring
+```
+pm2 ls
+pm2 monit
+pm2 show orbital-market-api
+```
+
+Restart / Stop / Delete
+```
+pm2 restart orbital-market-api
+pm2 stop orbital-market-api
+pm2 delete orbital-market-api
+```
+
