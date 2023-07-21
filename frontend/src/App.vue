7e6d1362bb560fdecc4cd3061ffc3e4a92d7d43e
Hugo ATTAL <4563971+hugoattal@users.noreply.github.com>
2023-07-21 16:51:06 +0200

    Remove produchunt bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,11 @@
 <template>
-    <OProductHuntBar />
     <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import OProductHuntBar from "@/components/elements/OProductHuntBar.vue";
 
 export default defineComponent({
-    components: { OProductHuntBar }
 });
 </script>
 
