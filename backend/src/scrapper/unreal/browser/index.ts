0b8e8aafcc032e5b60d3a2904d198896bfb7f774
Hugo ATTAL <4563971+hugoattal@users.noreply.github.com>
2024-08-19 13:31:01 +0200

    Remove request restry

--- backend/src/scrapper/unreal/browser/index.ts
+++ backend/src/scrapper/unreal/browser/index.ts
@@ -1,6 +1,5 @@
 import puppeteer, { Page } from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import { timeout } from "@/utils/lib";
 
 let page: Page;
 
@@ -20,24 +19,8 @@ export async function makeRequest(url: string) {
     const response = await page.goto(url);
     let json = "";
 
-    let tryFetch = 5;
-
-    while (tryFetch--) {
-        try {
-            json = await response.text();
-            json = JSON.parse(json);
-            tryFetch = 0;
-            break;
-        }
-        catch (error) {
-            console.log(error);
-            console.log(json);
-            await Promise.race([
-                timeout(10 * 1000),
-                page.waitForNavigation({ waitUntil: "networkidle0" })
-            ]);
-        }
-    }
+    json = await response.text();
+    json = JSON.parse(json);
 
     return json;
 }
