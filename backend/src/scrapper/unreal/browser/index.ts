f29ca5e5c925d4f296ee82d967a8af048733e11a
hugoattal <4563971+hugoattal@users.noreply.github.com>
2025-04-17 19:50:46 +0200

    Fix conccurent fetch

--- backend/src/scrapper/unreal/browser/index.ts
+++ backend/src/scrapper/unreal/browser/index.ts
@@ -1,7 +1,9 @@
 import puppeteer, { Page } from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import { Mutex } from "async-mutex";
 
 let page: Page;
+export const navigationMutex = new Mutex();
 
 async function initBrowser() {
     puppeteer.use(StealthPlugin());
@@ -12,17 +14,28 @@ async function initBrowser() {
 }
 
 export async function makeRequest(url: string): Promise<unknown> {
-    if (!page) {
-        await initBrowser();
+    const mutexRelease = await navigationMutex.acquire();
+
+    try {
+        if (!page) {
+            await initBrowser();
+        }
+
+        const response = await page.goto(url);
+        let json = "";
+
+        json = await response.text();
+        json = JSON.parse(json);
+
+        return json;
     }
-
-    const response = await page.goto(url);
-    let json = "";
-
-    json = await response.text();
-    json = JSON.parse(json);
-
-    return json;
+    catch (error) {
+        console.error(`Error making request to ${ url }:`, error);
+        throw error;
+    }
+    finally {
+        mutexRelease();
+    }
 }
 
 export async function getRedirect(url: string): Promise<string> {
