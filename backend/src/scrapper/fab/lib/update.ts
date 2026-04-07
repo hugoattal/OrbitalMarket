230206349adec00e8d9ff2431c51bb11184ba3f8
hugoattal <4563971+hugoattal@users.noreply.github.com>
2026-04-07 17:44:40 +0200

    Add verification ratio check

--- backend/src/scrapper/fab/lib/update.ts
+++ backend/src/scrapper/fab/lib/update.ts
@@ -204,7 +204,7 @@ function getEngine(product: Record<string, unknown>) {
 export function addComputed(product: TProductModel) {
     product.description.long = product.description.long?.replaceAll(`<a href="`, `<a target="_blank" href="`) || "";
     const isBoosted = getIsBoosted(product.description.long);
-    const score = computeScore(product.review.rating, product.review.count, new Date(product.releaseDate), product.price.value === 0, isBoosted);
+    const score = computeScore(product.review.rating, product.review.count, new Date(product.releaseDate), product.price.value === 0, isBoosted, product.meta?.verificationRatio);
 
     product.computed = {
         embeddedContent: getEmbeddedContent(product.description.long),
